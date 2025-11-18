5cf67a48c35ca740ea473ce2ad3f5d3b39079efd
JavizzzG <javierstivengalvis2008@gmail.com>
2025-11-18 00:32:47 -0500

    Chage to allowed origins

--- src/main/java/com/carsPractice1/config/CorsConfig.java
+++ src/main/java/com/carsPractice1/config/CorsConfig.java
@@ -12,7 +12,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOrigins("https://carspractice1-frontend.onrender.com/")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true);
